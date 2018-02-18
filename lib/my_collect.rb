ec6ab4e243313472bd59f2754ba1b72edbe887ce
Shea Kusiak <shkusiak@gmail.com>
2018-02-18 17:45:10 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
 
 
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+my_collect(array) {|name|
+  name.split(" ").first
+}
